016971c45b59a3f3dc172ae24e73d09e44e1fed4
gourav06111993 <gouravmandliya23@gmail.com>
2021-09-21 19:51:11 +0530

    initial commit

---
+++ EasyScrollView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyScrollView
+//
+//  Created by Gourav on 21/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
